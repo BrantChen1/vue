8f071fbbf68c582fe6f8ae3790396596317b5530
junwei.h <lovely4399@163.com>
2020-02-04 11:11:50 +0800

    框架1.0

---
+++ src/locale/lang/zh-CN.js
@@ -0,0 +1,6 @@
+export default {
+  home: '首页',
+  login: '登录',
+  manage: '菜单1',
+  product: '菜单一'
+};
---
+++ src/view/config/product/manage-list/list.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="">
+     123
+   </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {};
+  },
+  components: {},
+  mounted: {},
+  method: {}
+};
+</script>
+
+<style scoped lang="css">
+</style>
